1819342c5531feec87293d3a9e3f5921c25bd2a8
YOGESH-CHOUDHARY <me.yogesh24@gmail.com>
2019-04-25 12:05:39 +0530

    Add files via upload

--- app/src/main/java/com/yogesh/constrainttrial/NeverEndingPractice.java
+++ app/src/main/java/com/yogesh/constrainttrial/NeverEndingPractice.java
@@ -1,13 +1,30 @@
 package com.yogesh.constrainttrial;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class NeverEndingPractice extends AppCompatActivity {
+public class NeverEndingPractice extends AppCompatActivity implements View.OnClickListener {
+
+    Button practice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_never_ending_practice);
+
+        practice=findViewById(R.id.btn_practice);
+
+        practice.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+       startActivity(new Intent(NeverEndingPractice.this,FragmentMainActivity.class));
+
     }
 }
