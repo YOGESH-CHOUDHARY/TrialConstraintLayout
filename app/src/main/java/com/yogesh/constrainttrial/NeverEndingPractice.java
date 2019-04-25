d6b913ae5838d769aaaa68b262e38be12c1f66dc
YOGESH-CHOUDHARY <me.yogesh24@gmail.com>
2019-04-25 11:46:56 +0530

    Add files via upload

---
+++ app/src/main/java/com/yogesh/constrainttrial/NeverEndingPractice.java
@@ -0,0 +1,13 @@
+package com.yogesh.constrainttrial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NeverEndingPractice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_never_ending_practice);
+    }
+}
